6f83fd318c8c8fc85360754a08876b47a003638f
rlaalsrl715 <minkik715@gmail.com>
2024-03-03 18:52:17 +0900

    dynamic parameter

---
+++ src/main/java/call_by_value/Call.java
@@ -0,0 +1,37 @@
+package call_by_value;
+
+
+public class Call {
+    public static void main(String[] args) {
+        //출력을 예상해 보자
+        int primitiveNumber = 1;
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        NumberSet ns = new NumberSet();
+        change(primitiveNumber, numbers, ns);
+        System.out.println(primitiveNumber);
+        System.out.println(numbers[0]);
+        System.out.println(ns.primitiveNumber);
+        System.out.println(ns.numbers[0]);
+        change(ns.primitiveNumber, ns.numbers, null);
+        System.out.println(ns.primitiveNumber);
+        System.out.println(ns.numbers[0]);
+
+
+    }
+
+    public static class NumberSet {
+        int primitiveNumber = 1;
+        Integer wrapperNumber = 2;
+        int[] numbers = {1, 2, 3, 4, 5};
+    }
+
+    public static void change(int primitiveNumber,  int[] numbers, NumberSet ns) {
+        primitiveNumber = primitiveNumber + 1;
+        numbers[0] = numbers[0] + 1;
+        if (ns != null) {
+            ns.primitiveNumber = ns.primitiveNumber + 1;
+            ns.numbers[0] = ns.numbers[0] + 1;
+        }
+    }
+}
---
+++ src/main/java/dynamic_parameter/Apple.java
@@ -0,0 +1,2 @@
+package dynamic_parameter;public class Apple {
+}
